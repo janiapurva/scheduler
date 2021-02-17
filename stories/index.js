a6f07de47e2d9241add50f4e2e2d68ae21e662e5
janiapurva <jani.apurva@yahoo.com>
2021-02-16 23:00:46 -0500

    added feature of appoint show

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ Fragment }from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 
@@ -153,6 +154,22 @@ storiesOf("Button", module)
         })
         .add("Appointment", () => <Appointment />)
         .add("Appointment with Time", () => <Appointment time="12pm" />)
+        .add("Appointment Empty", () => (
+          <Fragment>
+            <Appointment id={1} time="12pm" />
+            <Appointment id="last" time="1pm" />
+          </Fragment>
+        ))
+        .add("Appointment Booked", () => (
+          <Fragment>
+            <Appointment
+              id={1}
+              time="12pm"
+              interview={{ student: "Lydia Miller-Jones", interviewer }}
+            />
+            <Appointment id="last" time="1pm" />
+          </Fragment>
+        ))
         .add("Header", () => <Header time="12pm" />)
         .add("Empty", () => <Empty onAdd={action("onAdd")} />)
         .add('Show', () => ( 
@@ -180,3 +197,20 @@ storiesOf("Button", module)
               onClose={action('onClose')}
             />
           ))
+          .add('Form - edit', () => (
+            <Form 
+              name="Bob"
+              interviewers={interviewers}
+              value={3}
+              onSave={action('onSave')}
+              onCancel={action('onCancel')}
+            />
+          ))
+          .add('Form - create', () => (
+            <Form
+              interviewers={interviewers}
+              onSave={action('onSave')}
+              onCancel={action('onCancel')}
+            />
+          ))    
+          
